2f206d271a2bbcfd77ee391f10b6557665a47e6f
Wexop <mattheurtin@gmail.com>
2023-12-19 22:21:50 +0100

    selection

--- pokedex/views.py
+++ pokedex/views.py
@@ -50,31 +50,24 @@ def fight(request):
 def editTeam(request):
     name = request.GET.get("name")
     id = request.GET.get("id")
+    selected = request.GET.get("selected")
 
     if not id:
         newPokeTeam = Pokemon_team.objects.create()
+
         id = newPokeTeam.id
         return redirect(f"/editTeam/?id={id}")
 
     pokemonTeam = Pokemon_team.objects.get(id=id)
 
-    team = {
+    team  = {
+            "pokemon1": Pokemon.objects.get(pokemonId=pokemonTeam.pokemon1),
+            "pokemon2": Pokemon.objects.get(pokemonId=pokemonTeam.pokemon2),
+            "pokemon3": Pokemon.objects.get(pokemonId=pokemonTeam.pokemon3),
+            "pokemon4": Pokemon.objects.get(pokemonId=pokemonTeam.pokemon4),
+            "pokemon5": Pokemon.objects.get(pokemonId=pokemonTeam.pokemon5),
+            "pokemon6": Pokemon.objects.get(pokemonId=pokemonTeam.pokemon6),
         }
-
-    if pokemonTeam.pokemon1 > 0:
-        team["pokemon1"] = Pokemon.objects.get(pokemonId=pokemonTeam.pokemon1)
-    if pokemonTeam.pokemon2 > 0:
-        team["pokemon2"] = Pokemon.objects.get(pokemonId=pokemonTeam.pokemon2)
-    if pokemonTeam.pokemon3 > 0:
-        team["pokemon3"] = Pokemon.objects.get(pokemonId=pokemonTeam.pokemon3)
-    if pokemonTeam.pokemon1 > 0:
-        team["pokemon4"] = Pokemon.objects.get(pokemonId=pokemonTeam.pokemon4)
-    if pokemonTeam.pokemon4 > 0:
-        team["pokemon5"] = Pokemon.objects.get(pokemonId=pokemonTeam.pokemon5)
-    if pokemonTeam.pokemon5 > 0:
-        team["pokemon1"] = Pokemon.objects.get(pokemonId=pokemonTeam.pokemon1)
-    if pokemonTeam.pokemon6 > 0:
-        team["pokemon6"] = Pokemon.objects.get(pokemonId=pokemonTeam.pokemon6)
 
 
     pokemons = Pokemon.objects.all()
@@ -84,6 +77,8 @@ def editTeam(request):
         "pokemons": pokemons,
         "team": team,
         "id": id,
+        "selected": selected,
+        "name": name
     }
     return render(request, "Combat/teamEdit.html", context)
 
